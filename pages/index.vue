037eac0b216a24495defdfd0a7b18b8ee67c7f09
Kevin <k.mazzoni@reply.it>
2023-05-26 16:58:09 +0200

    Primo commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script>
+    alert('Displayed')
+</script>
+
+<template>
+    <h1>
+        This is my nuxt project
+    </h1>
+    <p class="paragraph">
+        This is the descriptin of the page
+    </p>
+</template>
+
+<style>
+    .paragraph{
+        background-color: red;
+    }
+</style>
---
+++ pages/something.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        Scrivo qualcosa
+    </div>
+    <NuxtLink to="/">Link to the index</NuxtLink>
+</template>
