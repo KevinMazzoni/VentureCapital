bded7490c2428c4ee18da3d26ec2626a8f158cb3
lucap498 <lucap498@gmail.com>
2023-06-06 17:23:33 +0200

    add skeleton of new files with links

---
+++ pages/project.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                One of three columns
+            </div>
+            <div class="col-3">
+                One of three columns
+            </div>
+            <div class="col-4">
+                One of three columns
+            </div>
+        </div>
+    </div>
+</template>
