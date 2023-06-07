8f933a82007218fb67fd44a9e5057b9d5c532d0a
lucap498 <lucap498@gmail.com>
2023-06-07 18:35:32 +0200

    feat: pisco's commit

--- pages/project.vue
+++ pages/project.vue
@@ -1,18 +1,32 @@
 <template>
-    <div>
-        
-    </div>
+    <img src="~/assets/projects/poliigon.jpeg">
     <div class="container">
         <div class="row">
-            <div class="col-2">
-                One of three columns
-            </div>
-            <div class="col-3">
-                One of three columns
-            </div>
-            <div class="col-4">
-                One of three columns
+            <div class="col-6 description">
+                Poliigon provides high quality 3D assets that support the world’s leading 3D artists.
+                Through cutting-edge 3D creation processes, we deliver photoreal, ultra-high-quality    
+                textures, models, brushes and HDRIs for archviz, product rendering, game design, vfx and animation.
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+    img{
+        width: 100%;
+    }
+
+    .title{
+        font-size: 40px;
+        font-family: emoji;
+    }
+
+    .description{
+        font-size: 40px;
+        font-family: century-gothic, sans-serif;
+    }
+
+    .container{
+        margin-bottom: 5%;
+    }
+</style>
