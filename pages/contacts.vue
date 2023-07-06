790c168567fcb74250883c7750885643b6b5202c
gabrir99 <gabri.ruini@gmail.com>
2023-07-06 18:36:56 +0200

    feat: started contact us

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,20 +1,55 @@
 <template>
-    <div>
-        333 33333333
-        mail@mail.ma
-    </div>
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <img src="~/public/IntelliHire.jpg"> 
-            </div>
+      <div class="row mb-4">
+        <div class="col-7 outer-container ">
+          <div class="title mb-4">IF YOU WANT TO KNOW MORE</div>
+          <div class="row-lg mb-4 contacts">
+            Send us an email at: <a href="info@gloriousKoalas.com">info@gloriousKoalas.com</a>
+          </div>
+          <div class="row-lg mb-4 contacts">
+            Search us on <a href="https://it.linkedin.com/company/microsoft">linkedin</a>
+          </div>
         </div>
+        <div class="col-lg container-submit-projects">
+            <div class="title">Submit your project </div>
+            <form>
+              <div class="form-group">
+                <label for="companyName">Company Name:</label>
+                <input type="text" id="companyName" name="companyName" class="form-control" />
+              </div>
+              <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" class="form-control" />
+              </div>
+              <div class="form-group">
+                <label for="activityDescription">Activity Description:</label>
+                <textarea id="activityDescription" name="activityDescription" class="form-control"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary mt-4">Submit</button>
+            </form>
+        </div>
+      </div>
+      <div class="row">
+      <div class="col-8">
+        <div class="third-section">
+         AAAA
+        </div>
+      </div>
     </div>
+  </div>
 </template>
-
-<script setup>
-
-</script>
-
 <style>
+    a:link{
+        color: darkblue;
+        background-color: transparent; 
+        text-decoration: none;
+        text-size-adjust: initial;
+    }
+    .contacts{
+        font-size: large;
+    }
+    title{
+        font-size: medium;
+        font-family: Georgia, 'Times New Roman', Times, serif;
+    }
 </style>
