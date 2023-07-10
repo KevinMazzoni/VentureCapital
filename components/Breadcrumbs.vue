6a4ba3ddd76345c846ee7ea4964614d11842569f
gabrir99 <gabri.ruini@gmail.com>
2023-07-10 03:33:12 +0200

    feat: add Beadcrumbs in all pages

---
+++ components/Breadcrumbs.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row">
+      <div class="crumbs">
+        <span class="crumbs-text"> > {{ crumbs }} </span>
+      </div>
+    </div>
+  </template>
+  
+<script>
+export default {
+    computed: {
+        crumbs() {
+            return this.$route.name
+        },
+    },
+}
+</script>
+
+<style scoped>
+.crumbs {
+  background-color: rgb(70, 107, 145);
+  color: white;
+}
+
+.crumbs-text {
+  margin-left: 5%;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
     <TheHeader />
+    <Breadcrumbs class="mt-2 mb-2"></Breadcrumbs>
     <div class="slot">
         <slot />
     </div>
     <TheFooter />
 </template>
+
+<script setup>
+import Breadcrumbs from '../components/Breadcrumbs.vue';
+</script>
