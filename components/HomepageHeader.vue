1efa64d5363773bdfc4102fee7ff4a6f723da9b3
gabrir99 <gabri.ruini@gmail.com>
2023-07-10 22:42:12 +0200

    feat: add homepage header

---
+++ components/HomepageHeader.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="container">
+        <div class="row landmarksrow minwidth">
+            <div class="col-12">
+                <AnotherHeader/>
+            </div>
+        </div>
+        <div class="row logorow minwidth">
+            <div class="col-12">
+                <NuxtLink to="/">
+                    <img src="../assets/GloriousKoalasLogoBW.png" class="image" alt="Logo of Glorious Koalas, with a stylized Koala, and the motto 'We believe in you, we invest in you'">
+                </NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+    .container{
+        position: absolute;
+        height: 100%;
+        z-index: 10000;
+    }
+
+    .landmarksrow{
+        position: absolute;
+        width: 100%;
+    }
+    .logorow{
+        
+        align-items: center;
+        display: flex;
+        min-height: 100%;
+    }
+    .col-12{
+        display: flex;
+        justify-content: center;
+    }
+
+    .image{
+        width: 100%;
+        min-width: 200px;
+        height: auto;
+        padding-top: 4px;
+    }
+
+    nav {
+        gap: 10vh;
+    }
+
+    .landmark {
+        color:white;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        font-weight: 330;
+        font-size: 3vh;
+        text-decoration: none;
+        position: relative;
+    }
+
+    .landmark::after {
+        content: '';
+        position: absolute;
+        width: 100%;
+        transform: scaleX(0);
+        height: 2px;
+        bottom: 0;
+        left: 0;
+        background-color: #0087ca;
+        transform-origin: bottom right;
+        transition: transform 0.25s ease-out;
+    }
+
+    .landmark:hover::after {
+        transform: scaleX(1);
+        transform-origin: bottom left;
+    }
+
+    .minwidth{
+        min-width: 200px;
+    }
+</style>
